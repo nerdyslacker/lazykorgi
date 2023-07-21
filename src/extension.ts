4eba83ed2e92ec5f33054b862fba398f04cabcad
Karen Sargsyan <karen.sargsyan@topsoft.am>
2023-07-21 13:25:26 +0400

    added override-ownership and fallback-on-replace args to the command

--- src/extension.ts
+++ src/extension.ts
@@ -6,7 +6,7 @@ import * as vscode from 'vscode';
 /**
  * Korgi apply action.
  */
- async function korgiApply(uri: vscode.Uri, dry: boolean, skipDeps: boolean): Promise<vscode.TextEditor | undefined> {
+ async function korgiApply(uri: vscode.Uri, dry: boolean, skipDeps: boolean, kappArgs: Array<string>): Promise<vscode.TextEditor | undefined> {
 	const oldPath = uri.fsPath;
 	const oldPathParsed = path.parse(oldPath);
 	const oldPathStats = await fs.stat(oldPath);
@@ -27,7 +27,14 @@ import * as vscode from 'vscode';
 	const appNamespace = array[array.length - 3];
 	const appEnv = array[array.length - 4];
 
-	const command = `korgi apply -e ${appEnv} -n ${appNamespace} ${appGroup} -a ${appName} --isolate=true --kapp-args="--diff-changes" --dry-run=${dry} --skip-deps=${skipDeps}`;
+	let command = `korgi apply -e ${appEnv} -n ${appNamespace} ${appGroup} -a ${appName} --isolate=true --kapp-args="--diff-changes" --dry-run=${dry} --skip-deps=${skipDeps}`;
+	
+	if(kappArgs && kappArgs.length > 0){
+		kappArgs.forEach(arg => {
+			command += ` --kapp-args="${arg}"`;
+		});
+	}
+
 	if(vscode.window.terminals.length === 0){
 		const newTerminal = vscode.window.createTerminal(`lazykorgi terminal`);
 		newTerminal.show();
@@ -80,50 +87,82 @@ import * as vscode from 'vscode';
 }
 
 export function activate(context: vscode.ExtensionContext) {
-	const dryCommand = vscode.commands.registerCommand('lazykorgi.dryrun', (uri: vscode.TextDocument | vscode.Uri, dry: boolean, skipDeps: boolean) => {
+	const dryCommand = vscode.commands.registerCommand('lazykorgi.dryrun', (uri: vscode.TextDocument | vscode.Uri, dry: boolean, skipDeps: boolean, kappArgs: Array<string>) => {
 		if (!uri || !(<vscode.Uri>uri).fsPath) {
 			const editor = vscode.window.activeTextEditor;
 			if (!editor) {
 				return;
 			}
 
-			return korgiApply(<vscode.Uri>editor.document.uri, true, true);
+			return korgiApply(<vscode.Uri>editor.document.uri, true, true, []);
 		}
 
-		return korgiApply(<vscode.Uri>uri, true, true);
+		return korgiApply(<vscode.Uri>uri, true, true, []);
 	});
 
 	context.subscriptions.push(dryCommand);
 
-	const applyCommand = vscode.commands.registerCommand('lazykorgi.apply', (uri: vscode.TextDocument | vscode.Uri, dry: boolean, skipDeps: boolean) => {
+	const applyCommand = vscode.commands.registerCommand('lazykorgi.apply', (uri: vscode.TextDocument | vscode.Uri, dry: boolean, skipDeps: boolean, kappArgs: Array<string>) => {
 		if (!uri || !(<vscode.Uri>uri).fsPath) {
 			const editor = vscode.window.activeTextEditor;
 			if (!editor) {
 				return;
 			}
 
-			return korgiApply(<vscode.Uri>editor.document.uri, false, false);
+			return korgiApply(<vscode.Uri>editor.document.uri, false, false, []);
 		}
 
-		return korgiApply(<vscode.Uri>uri, false, false);
+		return korgiApply(<vscode.Uri>uri, false, false, []);
 	});
 
 	context.subscriptions.push(applyCommand);
 
-	const applyWithSkipCommand = vscode.commands.registerCommand('lazykorgi.applyskipping', (uri: vscode.TextDocument | vscode.Uri, dry: boolean, skipDeps: boolean) => {
+	const applyWithSkipCommand = vscode.commands.registerCommand('lazykorgi.applyskipping', (uri: vscode.TextDocument | vscode.Uri, dry: boolean, skipDeps: boolean, kappArgs: Array<string>) => {
 		if (!uri || !(<vscode.Uri>uri).fsPath) {
 			const editor = vscode.window.activeTextEditor;
 			if (!editor) {
 				return;
 			}
 
-			return korgiApply(<vscode.Uri>editor.document.uri, false, true);
+			return korgiApply(<vscode.Uri>editor.document.uri, false, true, []);
 		}
 
-		return korgiApply(<vscode.Uri>uri, false, true);
+		return korgiApply(<vscode.Uri>uri, false, true, []);
 	});
 
 	context.subscriptions.push(applyWithSkipCommand);
+
+	const applyWithOverrideOwnershipCommand = vscode.commands.registerCommand('lazykorgi.applyoverrideownership', (uri: vscode.TextDocument | vscode.Uri, dry: boolean, skipDeps: boolean, kappArgs: Array<string>) => {
+		let kappArguments = ["--dangerous-override-ownership-of-existing-resources=true"];
+		if (!uri || !(<vscode.Uri>uri).fsPath) {
+			const editor = vscode.window.activeTextEditor;
+			if (!editor) {
+				return;
+			}
+
+			return korgiApply(<vscode.Uri>editor.document.uri, false, true, kappArguments);
+		}
+
+		return korgiApply(<vscode.Uri>uri, false, true, kappArguments);
+	});
+
+	context.subscriptions.push(applyWithOverrideOwnershipCommand);
+
+	const applyWithRecreateOverrideOwnershipCommand = vscode.commands.registerCommand('lazykorgi.applyrecreateoverrideownership', (uri: vscode.TextDocument | vscode.Uri, dry: boolean, skipDeps: boolean, kappArgs: Array<string>) => {
+		let kappArguments = ["--dangerous-override-ownership-of-existing-resources=true", "--apply-default-update-strategy=fallback-on-replace"];
+		if (!uri || !(<vscode.Uri>uri).fsPath) {
+			const editor = vscode.window.activeTextEditor;
+			if (!editor) {
+				return;
+			}
+
+			return korgiApply(<vscode.Uri>editor.document.uri, false, true, kappArguments);
+		}
+
+		return korgiApply(<vscode.Uri>uri, false, true, kappArguments);
+	});
+
+	context.subscriptions.push(applyWithRecreateOverrideOwnershipCommand);
 
 	const deleteCommand = vscode.commands.registerCommand('lazykorgi.delete', (uri: vscode.TextDocument | vscode.Uri) => {
 		if (!uri || !(<vscode.Uri>uri).fsPath) {
